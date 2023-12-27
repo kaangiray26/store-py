1bbf7fca2203bf02e69ec9ba626adbd320161ce3
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2023-12-27 22:12:46 +0300

    progress bar

--- main.py
+++ main.py
@@ -3,29 +3,17 @@
 
 import os
 import json
-import requests
+import httpx
 import argparse
+from tqdm import tqdm
+from tqdm.utils import CallbackIOWrapper
 
 class Store:
-    def __init__(self):
-        # Check for config file
-        if not os.path.isfile("config.json"):
-            with open("config.json", "w") as f:
-                json.dump({}, f, indent=4)
-            print("Empty config file created!")
-        
-        with open("config.json") as f:
+    def __init__(self):        
+        with open(os.path.join(os.path.expanduser('~'),'.config/store-py/config.json')) as f:
             self.config = json.load(f)
     
-    def handle_args(self, args):
-        # Override config with args
-        if args.server:
-            self.config["server"] = args.server
-        if args.username:
-            self.config["username"] = args.username
-        if args.password:
-            self.config["password"] = args.password
-        
+    def handle_args(self, args):        
         # Check for server
         if not self.config["server"]:
             raise Exception("Server not specified!")
@@ -58,19 +46,23 @@ class Store:
             self.replace_file(args.replace)
             return
         
+        if args.search:
+            self.search_file(args.search)
+            return
+        
     def get_file(self, filename):
-        r = requests.get(f"{self.config['server']}/get/{filename}")
+        r = httpx.get(f"{self.config['server']}/{filename}")
+        
         if r.status_code != 200:
             raise Exception("File not found!")
         
+        # Save file
         with open(filename, 'wb') as f:
             f.write(r.content)
         
         print("File saved to: " + filename)
         
-    def replace_file(self, args):
-        print("Replacing file: " + args[0] + " with " + args[1])
-        
+    def replace_file(self, args):        
         # Check if file exists
         if not os.path.isfile(args[1]):
             raise Exception("File does not exist!")
@@ -80,55 +72,94 @@ class Store:
         files = {'file': open(args[1], 'rb')}
         headers = {'username': self.config['username'], 'password': self.config['password']}
                 
-        r = requests.put(server, files=files, headers=headers)
-        print(r.status_code, r.json())
+        r = httpx.put(server, files=files, headers=headers)
+        if r.status_code != 200:
+            raise Exception("Error replacing file!")
+        
+        print(r.json())
         
     def list_files(self):
-        print("Listing files")
-        
-        # List files from server
         server = f"{self.config['server']}/list"
         headers = {'username': self.config['username'], 'password': self.config['password']}
         
-        r = requests.get(server, headers=headers)
-        print(r.status_code, r.json())
+        # Get files from server
+        r = httpx.get(server, headers=headers)
+        if r.status_code != 200:
+            raise Exception("Error listing files!")
+        
+        # Print files
+        data = r.json()
+        if not len(data['response']):
+            print("No owned files!")
+            return
+        
+        print("Files:")
+        for object in r.json()['response']:
+            print(object)
         
     def delete_file(self, filename):
-        print("Deleting file: "+ filename)
-        
         # Delete file from server
         server = f"{self.config['server']}/delete/{filename}"
         headers = {'username': self.config['username'], 'password': self.config['password']}
         
-        r = requests.delete(server, headers=headers)
-        print(r.status_code, r.json())
-            
-    def add_file(self, filepath):
-        print("Adding file: " + filepath)
+        r = httpx.delete(server, headers=headers)
+        if r.status_code != 200:
+            raise Exception("Error deleting file!")
         
+        print(r.json())
+            
+    def add_file(self, args):
         # Check if file exists
-        if not os.path.isfile(filepath):
+        if not os.path.isfile(args[0]):
             raise Exception("File does not exist!")
+        
+        # Set data to send
+        data = {}
+        if len(args) == 2:
+            data['title'] = args[1]
         
         # Put file to server
         server = f"{self.config['server']}/add"
-        files = {'file': open(filepath, 'rb')}
         headers = {'username': self.config['username'], 'password': self.config['password']}
+        
+        file_size = os.path.getsize(args[0])
+        
+        # Upload file with tqdm progress bar
+        with open(args[0], 'rb') as f:
+            with tqdm(desc=f"Uploading", total=file_size, unit='B', unit_scale=True, unit_divisor=1024) as t:
+                wrapper = CallbackIOWrapper(t.update, f, 'read')
+                r = httpx.put(server, files={'file': wrapper}, headers=headers, data=data)
                 
-        r = requests.put(server, files=files, headers=headers)
-        print(r.status_code, r.json())
+                # Clear progress bar
+                t.close()
+                
+                if r.status_code != 200:
+                    print(r.content)
+                    raise Exception("Error adding file!")
+                
+                # Print response
+                print(r.json())
+                
+    def search_file(self, query):
+        server = f"{self.config['server']}/search"
+        headers = {'username': self.config['username'], 'password': self.config['password']}
+        
+        # Search files from server
+        r = httpx.get(server, headers=headers, params={'title': query})
+        if r.status_code != 200:
+            raise Exception("Error replacing file!")
+        
+        print(r.json())
 
 if __name__ == "__main__":
     s = Store()
     parser = argparse.ArgumentParser(description='kaangiray26/store-py')
-    parser.add_argument('-a', '--add', help='Add file to the store', metavar='file_path')
+    parser.add_argument('-a', '--add', help='Add file to the store', nargs='+', metavar=('file_path', 'title'))
     parser.add_argument('-d', '--delete', help='Delete file from the store', metavar='file_id')
     parser.add_argument('-l', '--list', help='List files in the store', action='store_true')
     parser.add_argument('-r', '--replace', help='Replace file in the store', nargs=2, metavar=('file_id', 'file_path'))
     parser.add_argument('-g', '--get', help='Get file from the store', metavar='file_id')
-    parser.add_argument('-s', '--server', help='Server', metavar='server_url')
-    parser.add_argument('-u', '--username', help='Username', metavar='username')
-    parser.add_argument('-p', '--password', help='Password', metavar='password')
+    parser.add_argument('-s', '--search', help='Search file from the store', metavar='query')
     
     args = parser.parse_args()
     s.handle_args(args)
